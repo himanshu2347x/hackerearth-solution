71e55418882ff9d955bb4ea4d9ab24f46b79c742
himanshu2347x <himanshu.sinha392@gmail.com>
2023-03-15 10:54:33 +0530

    Added number of steps problem

---
+++ Number of steps/README.md
@@ -0,0 +1,2 @@
+# [Problem](https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/make-all-equal-90a21ab2/)
+You are given two arrays `a1, a2,..., an` and `b1, b2,...,bn`. In each step, you can set `ai = ai - bi` if `ai>=bi` Determine the minimum number of steps that are required to make all a's equal.
---
+++ Number of steps/solution.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count = 0, n;
+    cin >> n;
+    int a[n];
+    int b[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int min = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    int i = 0;
+    while (i < n)
+
+    {
+
+        // c=0;
+
+        while (a[i] > min)
+
+        {
+
+            a[i] = a[i] - b[i];
+
+            count++;
+        }
+
+        if (a[i] < min)
+
+        {
+
+            min = a[i];
+
+            i = 0;
+        }
+
+        else
+
+            if (a[i] < 0)
+
+        {
+
+            count = -1;
+
+            break;
+        }
+
+        else
+
+            i++;
+    }
+
+    cout << count << endl;
+}
